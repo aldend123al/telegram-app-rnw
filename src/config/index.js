3f6c06e55992fff2168db35a448f47b8edcad785
aldend123al <aldend0143@gmail.com>
2024-08-23 01:20:43 +0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const BASE_API_URL =
   process.env.REACT_APP_BASE_API_URL ||
-  "https://acd98dbb-e5df-4955-901b-0d91985ef9a6-00-258xgd2e6gwmh.pike.replit.dev:3002/api/";
+  "https://https://renw-api-serv.onrender.com/api/";
